44c98937bfb273283dbeb8e0e898c1ddcfdc6ae4
Aayush-G18 <goyalaayush292@gmail.com>
2024-03-27 18:03:14 +0530

    .

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-4xl ">Hello</div>
+      <div className="text-slate-600 text-4xl ">Hello</div>
+      <Button className="">Click ME</Button>
     </main>
   );
 }
